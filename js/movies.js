b762972e1abe8b361817376b873126789d0d8b3f
jiamliang2 <62740908+jiamliang2@users.noreply.github.com>
2021-02-03 18:45:56 +0800

    added new file

---
+++ js/movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"img/casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Cassblanca",
+            cast:"Humphrey Bogart, Ingrid Bergmen",
+            director:"Michael Curtiz",
+            duration: 130
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"img/chocolate.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/example/video/bear.mp4",
+            name:"Chocolate",
+            cast:"Big Bunny, Little Fox",
+            director:"Forest King",
+            duration: 100
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"img/nature.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Nature",
+            cast:"Chris Leung, Tom Yu",
+            director:"Sandy Change",
+            duration: 100
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"img/bigbuck.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"BigBuck",
+            cast:"Peter Wong, Ken Lee",
+            director:"Mary Chan",
+            duration: 120
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"img/wildlife.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Wildlife",
+            cast:"Pranav Gray, Eman Bradley",
+            director:"Karol Marsden",
+            duration: 150
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"img/wonders.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name:"Wonders",
+            cast:"Anaya Lane, Evie-May Cohen",
+            director:"Hisham Doyle",
+            duration: 120
+        }
+    ]
+}
